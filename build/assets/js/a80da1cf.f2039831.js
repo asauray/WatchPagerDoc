4e354097dde3ca9ab817088483cf1f21da637f7d
Antoine Sauray <sauray.antoine@icloud.com>
2021-11-29 20:50:24 +0100

    add built site

---
+++ build/assets/js/a80da1cf.f2039831.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwatchpager_doc=self.webpackChunkwatchpager_doc||[]).push([[205],{4863:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/docusaurus","name":"docusaurus","count":4,"permalink":"/blog/tags/docusaurus"}')}}]);
